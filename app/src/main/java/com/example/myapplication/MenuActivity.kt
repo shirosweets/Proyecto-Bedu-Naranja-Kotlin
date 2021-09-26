fe3f37fdebc0527e5ae5f2967bb4497d1f6855ad
AgustinMDominguez <agustinmarcelodominguez@gmail.com>
2021-09-26 15:38:15 -0300

    Add top_nav_menu with help implemented and search button (not implemented)
    Co-authored-by: shirosweets <60824319+shirosweets@users.noreply.github.com>
    Co-authored-by: RicardoLopez9908 <57273461+RicardoLopez9908@users.noreply.github.com>

--- app/src/main/java/com/example/myapplication/MenuActivity.kt
+++ app/src/main/java/com/example/myapplication/MenuActivity.kt
@@ -1,13 +1,49 @@
 package com.example.myapplication
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MenuActivity : AppCompatActivity() {
+    private val helpUrl = "https://www.bedu.org/"
     private lateinit var menuNavigationBottom : BottomNavigationView
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.top_nav_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.top_menu_search -> {
+                Toast.makeText(
+                    this,
+                    "Search not implemented...",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+            R.id.top_menu_help -> {
+                val intent = Intent(Intent.ACTION_VIEW)
+                intent.data = Uri.parse(helpUrl)
+                startActivity(intent)
+            }
+            else -> {
+                Toast.makeText(
+                    this,
+                    "Functionality ${item.itemId} not implemented",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
