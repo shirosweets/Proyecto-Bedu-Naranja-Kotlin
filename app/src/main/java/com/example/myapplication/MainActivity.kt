562f7aaf7b7767b548c311b1a168f2d0ab98264b
Valentina Vispo <valentinavispounc@gmail.com>
2021-10-17 19:10:24 -0300

    Add first version of notification

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,26 +3,25 @@ package com.example.myapplication
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.navigation.findNavController
-import androidx.navigation.fragment.findNavController
 import android.content.Intent
-
-
-
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val sharedPreferences = this.getSharedPreferences("org.bedu.sharedpreferences", Context.MODE_PRIVATE)
+        val sharedPreferences = this.getSharedPreferences(
+            "org.bedu.sharedpreferences",
+            Context.MODE_PRIVATE
+        )
+
         if(sharedPreferences?.getBoolean("USER_ACCESS", false) == true){
             val intent = Intent(this, MenuActivity::class.java)
             startActivity(intent)
-        }else{
+        }
+        else{
             setContentView(R.layout.activity_main)
             supportActionBar?.hide()
         }
-
     }
 }
 
