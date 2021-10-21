9d513622610151396bd829a0d7030bee8cc02a1f
AgustinMDominguez <agustinmarcelodominguez@gmail.com>
2021-10-21 03:16:18 -0300

    Refactor HomeFragment and RegisterFragment

--- app/src/main/java/com/example/myapplication/HomeFragment.kt
+++ app/src/main/java/com/example/myapplication/HomeFragment.kt
@@ -1,9 +1,6 @@
 package com.example.myapplication
 
-import android.content.Context
-import android.content.SharedPreferences
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -22,35 +19,26 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 
 class HomeFragment : Fragment() {
-
     private lateinit var recycler: RecyclerView
     private lateinit var homeProgressBar: ProgressBar
-    private lateinit var sharedPreferences: SharedPreferences
     private val baseUrl = "https://fakestoreapi.com/"
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         recycler = view.findViewById(R.id.productRecyclerView)
         homeProgressBar = view.findViewById(R.id.homeProgressBar)
-        sharedPreferences = requireActivity().getSharedPreferences(
-            getString(R.string.login_shared_preference_file),
-            Context.MODE_PRIVATE
-        )
         getProducts(view)
     }
 
     private fun loadProductsSuccessfully(view:View, products : List<Product>){
-        val clickListener: (Product, FragmentNavigator.Extras) -> Unit = {
-                product, extras ->
+        val clickListener: (Product, FragmentNavigator.Extras) -> Unit = { product, extras ->
             val action = HomeFragmentDirections.actionHomeFragmentToProductDetailFragment(product)
             Navigation.findNavController(view).navigate(action,extras)
         }
@@ -60,6 +48,7 @@ class HomeFragment : Fragment() {
         homeProgressBar.visibility = View.INVISIBLE
     }
 
+
     private fun getRetrofit():Retrofit{
         return Retrofit.Builder()
             .baseUrl(baseUrl)
@@ -67,52 +56,51 @@ class HomeFragment : Fragment() {
             .build()
     }
 
-    private fun fetchFromAPI() {
-
-    }
-
-    private fun getProducts(view: View){
-        val isFetched: Boolean = sharedPreferences
+    private fun getProducts(view: View) {
+        val isFetched: Boolean = ConfigManager.prefs(requireActivity())
             .getBoolean(getString(R.string.pref_is_database_fetched), false)
         if (isFetched) {
             loadProductsSuccessfully(view, ProductDatabase.fetchAllProducts())
         } else {
-            CoroutineScope(Dispatchers.IO).launch {
-                val call = getRetrofit().create(APIService::class.java).getProducts("products")
-                val productsReceived = call.body()
-                activity?.runOnUiThread{
-                    if(call.isSuccessful) {
-                        productsReceived?.forEach {
-                            Log.v("MYDEBUG", it.toString())
-                            val amountAddedToCart = ProductDatabase
-                                .fetchProduct(it.id)
-                                ?.amountAddedToCart
-                                ?: 0
-                            ProductDatabase.addProduct(
-                                it.id,
-                                it.title,
-                                it.price,
-                                it.description,
-                                it.category,
-                                it.image,
-                                it.rating.count,
-                                it.rating.rate,
-                                amountAddedToCart
-                            )
-                        }
-                        sharedPreferences.edit().putBoolean(
-                            getString(R.string.pref_is_database_fetched),
-                            true
-                        ).apply()
-                        loadProductsSuccessfully(view, ProductDatabase.fetchAllProducts())
+            loadProductsFromAPI(view)
+        }
+    }
+
+    private fun loadProductsFromAPI(view: View) {
+        CoroutineScope(Dispatchers.IO).launch {
+            val call = getRetrofit().create(APIService::class.java).getProducts("products")
+            val productsReceived = call.body()
+            activity?.runOnUiThread {
+                if(call.isSuccessful) {
+                    productsReceived?.forEach {
+                        val amountAddedToCart = ProductDatabase
+                            .fetchProduct(it.id)
+                            ?.amountAddedToCart
+                            ?: 0
+                        ProductDatabase.addProduct(
+                            it.id,
+                            it.title,
+                            it.price,
+                            it.description,
+                            it.category,
+                            it.image,
+                            it.rating.count,
+                            it.rating.rate,
+                            amountAddedToCart
+                        )
                     }
-                    else {
-                        Toast.makeText(
-                            requireContext(),
-                            "Error al solicitar productos",
-                            Toast.LENGTH_SHORT
-                        ).show()
-                    }
+                    ConfigManager.prefs(requireActivity()).edit().putBoolean(
+                        getString(R.string.pref_is_database_fetched),
+                        true
+                    ).apply()
+                    loadProductsSuccessfully(view, ProductDatabase.fetchAllProducts())
+                }
+                else {
+                    Toast.makeText(
+                        requireContext(),
+                        "Error al solicitar productos",
+                        Toast.LENGTH_SHORT
+                    ).show()
                 }
             }
         }
